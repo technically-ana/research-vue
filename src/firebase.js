76b2c446d2d9252caebe507df446fb099b1cd61b
tech.ana <technically.ana@proton.me>
2025-03-05 21:25:57 +1300

    Got rid of credentials

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,6 @@ import { getFirestore, collection } from 'firebase/firestore'
 import { getAuth } from 'firebase/auth'
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDtfINbv2Ttr_lzOcZG4B2Cz1wDmtD4Ayc",
-    authDomain: "poll-system-vue.firebaseapp.com",
-    projectId: "poll-system-vue",
-    storageBucket: "poll-system-vue.firebasestorage.app",
-    messagingSenderId: "16485415567",
-    appId: "1:16485415567:web:23fd8a6a855c6547e08efa",
-    measurementId: "G-7P8L3C6QMT"
 };
 
 export const firebaseApp = initializeApp(firebaseConfig)
