e04e5bf1ee4c7b05b0504ce3a7893e42245970e4
tech.ana <technically.ana@proton.me>
2025-04-06 15:29:22 +1200

    deleted commented code

--- src/main.js
+++ src/main.js
@@ -5,13 +5,11 @@ import router from './router'
 import { VueFireDatabaseOptionsAPI } from 'vuefire'
 
 
-// the file we created above with `database`, `firestore` and other exports
 import { firebaseApp } from './firebase'
 
 const app = createApp(App)
 
 app.use(VueFire, {
-    // imported above but could also just be created here
     firebaseApp,
     modules: [
         VueFireAuth(),
